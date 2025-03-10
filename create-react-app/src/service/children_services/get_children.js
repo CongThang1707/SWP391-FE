dd0a0d0cd0b4390cdbfd5852b039d53de3fb7ce4
vutien2412 <tien0937170540@gmail.com>
2025-03-10 09:14:53 +0700

    CRUD child

--- create-react-app/src/service/children_services/get_children.js
+++ create-react-app/src/service/children_services/get_children.js
@@ -1,3 +1,4 @@
+//get_children.js
 import axios from 'axios';
 import API_URL from '../api_service.js';
 
@@ -31,5 +32,45 @@ export const getChildrenById = async (childId) => {
     return response.data;
   } catch (error) {
     console.error(error);
+  }
+};
+
+export const deleteChild = async (childId) => {
+  try {
+    const response = await axios.delete(`${API_URL}childrenAPI/deleteChild/{childId}?childId=${childId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting child:', error);
+    throw error;
+  }
+};
+
+export const updateChild = async (childId, updatedData) => {
+  try {
+    const response = await axios.put(`${API_URL}childrenAPI/updateChild/${childId}`, updatedData);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating child:', error);
+    throw error;
+  }
+};
+
+export const createChild = async (childData) => {
+  try {
+    const parentId = localStorage.getItem('userId');
+    if (!parentId) {
+      throw new Error('Parent ID not found in localStorage');
+    }
+
+    const response = await axios.post(`${API_URL}childrenAPI/createChild?parentId=${parentId}`, childData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error creating child:', error.response ? error.response.data : error.message);
+    throw error;
   }
 };
